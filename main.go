b328f508195b6e7a5dca057e44ce47c9237a1b95
steveljko <steveljko@proton.me>
2025-04-14 19:17:15 +0200

    add database migrations

--- main.go
+++ main.go
@@ -28,6 +28,38 @@ func init() {
   if err := db.Ping(); err != nil {
 		log.Fatalf("Failed to ping database: %v", err)
 	}
+
+  if err := runMigrations(); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+  }
+}
+
+// Run database migrations
+func runMigrations() error {
+  tables := []string {
+    `CREATE TABLE IF NOT EXISTS sheets (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );`,
+    `CREATE TABLE IF NOT EXISTS entries (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      sheet_id INTEGER NOT NULL,
+      start_time DATETIME,
+      end_time DATETIME,
+      note TEXT,
+      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (sheet_id) REFERENCES sheets(id)
+    );`,
+  }
+  
+  for _, tableSQL := range tables {
+    if _, err := db.Exec(tableSQL); err != nil {
+      return fmt.Errorf("Failed to create table: %w", err)
+    }
+  }
+
+  return nil
 }
 
 func main() {
