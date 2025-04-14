3fde030a3236f098a1228a4278271f01177a936b
steveljko <steveljko@proton.me>
2025-04-14 21:59:00 +0200

    add start and stop functionality for time tracking

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+  "time"
 	"fmt"
 	"log"
 	"os"
@@ -11,10 +12,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-  db *sql.DB
-  sheetName string
-)
+var db *sql.DB
 
 var rootCmd = &cobra.Command{
 	Use:   "timetick",
@@ -29,12 +27,14 @@ var sheetCmd = &cobra.Command{
   Short: "Change tracking sheet",
   Args:  cobra.ExactArgs(1),
   Run: func(cmd *cobra.Command, args []string) {
-    sheetName = args[0]
+    sheetName := args[0]
 
     if checkSheetExists(sheetName) {
+      setActiveSheet(sheetName);
       fmt.Printf("Changed sheet to: %s\n", sheetName)
     } else {
       createSheet(sheetName)
+      setActiveSheet(sheetName);
       fmt.Printf("Created and changed sheet to: %s\n", sheetName)
     }
   },
@@ -57,6 +57,74 @@ func createSheet(name string) {
 	}
 }
 
+// set active sheet by name
+func setActiveSheet(name string) {
+  // deactivate all sheets
+  _, err := db.Exec("UPDATE sheets SET active = 0")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // activate with name 
+  _, err = db.Exec("UPDATE sheets SET active = 1 WHERE name = ?", name)
+  if err != nil {
+    log.Fatal(err)
+  }
+}
+
+// get active sheet id
+func getActiveSheetId() (int64, error) {
+  var id int64
+  err := db.QueryRow("SELECT id FROM sheets WHERE active = 1").Scan(&id)
+  if err != nil {
+    if err == sql.ErrNoRows {
+      return 0, nil
+    }
+    return 0, err
+  }
+  return id, nil
+}
+
+var startCmd = &cobra.Command{
+  Use:   "start [note]",
+  Short: "Start tracking time",
+  Args:  cobra.ExactArgs(1),
+  Run: func(cmd *cobra.Command, args []string) {
+    note := args[0]
+    startTime := time.Now()
+
+    id, _ := getActiveSheetId()
+
+    _, err := db.Exec("INSERT INTO entries (sheet_id, start_time, note) VALUES (?, ?, ?)", id, startTime, note)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    fmt.Printf("Started tracking time with note: %s at %s\n", note, startTime)
+  },
+}
+
+var stopCmd = &cobra.Command{
+  Use:   "stop",
+  Short: "Stop tracking time",
+  Run: func(cmd *cobra.Command, args []string) {
+    endtime := time.Now()
+
+    var entryId int64
+    err := db.QueryRow("SELECT id FROM entries WHERE end_time IS NULL").Scan(&entryId)
+    if err != nil {
+      log.Fatalf("Error finding entry without end time: %v", err)
+    }
+
+    _, err = db.Exec("UPDATE entries SET end_time = ? WHERE id = ?", endtime, entryId)
+    if err != nil {
+      log.Fatalf("Error while updating end time to entry: %v", err)
+    }
+
+    fmt.Printf("Stopeed tracking time with entry: %d\n", entryId)
+  },
+}
+
 func init() {
 	dbPath := filepath.Join(os.Getenv("HOME"), ".local", "share", "timetick", "database.db")
 
@@ -67,7 +135,7 @@ func init() {
 
 	db, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
-    log.Fatal("Failed to open database: %v", err)
+    log.Fatalf("Failed to open database: %v", err)
 	}
 
   if err := db.Ping(); err != nil {
@@ -79,6 +147,8 @@ func init() {
   }
 
 	rootCmd.AddCommand(sheetCmd)
+	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(stopCmd)
 }
 
 // Run database migrations
@@ -87,6 +157,7 @@ func runMigrations() error {
     `CREATE TABLE IF NOT EXISTS sheets (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
+      active INTEGER NOT NULL DEFAULT 0,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     );`,
     `CREATE TABLE IF NOT EXISTS entries (
