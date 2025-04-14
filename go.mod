7f1f009a7fb14dafde28edcd4338d77c63f77af7
steveljko <steveljko@proton.me>
2025-04-14 19:59:58 +0200

    add cobra and command for sheet change

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module timetick
 
 go 1.24.1
 
-require github.com/mattn/go-sqlite3 v1.14.27
+require (
+	github.com/mattn/go-sqlite3 v1.14.27
+	github.com/spf13/cobra v1.9.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+)
--- main.go
+++ main.go
@@ -8,9 +8,54 @@ import (
 	"path/filepath"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/spf13/cobra"
 )
 
-var db *sql.DB
+var (
+  db *sql.DB
+  sheetName string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "timetick",
+	Short: "X",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to the Time Tracking App")
+	},
+}
+
+var sheetCmd = &cobra.Command{
+  Use:   "sheet [sheetname]",
+  Short: "Change tracking sheet",
+  Args:  cobra.ExactArgs(1),
+  Run: func(cmd *cobra.Command, args []string) {
+    sheetName = args[0]
+
+    if checkSheetExists(sheetName) {
+      fmt.Printf("Changed sheet to: %s\n", sheetName)
+    } else {
+      createSheet(sheetName)
+      fmt.Printf("Created and changed sheet to: %s\n", sheetName)
+    }
+  },
+}
+
+func checkSheetExists(name string) bool {
+	var exists bool
+	query := `SELECT EXISTS(SELECT 1 FROM sheets WHERE name = ?)`
+	err := db.QueryRow(query, name).Scan(&exists)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return exists
+}
+
+func createSheet(name string) {
+	_, err := db.Exec("INSERT INTO sheets (name) VALUES (?)", name)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func init() {
 	dbPath := filepath.Join(os.Getenv("HOME"), ".local", "share", "timetick", "database.db")
@@ -32,6 +77,8 @@ func init() {
   if err := runMigrations(); err != nil {
 		log.Fatalf("Failed to run migrations: %v", err)
   }
+
+	rootCmd.AddCommand(sheetCmd)
 }
 
 // Run database migrations
@@ -63,5 +110,8 @@ func runMigrations() error {
 }
 
 func main() {
-	fmt.Println("Hello!")
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
 }
